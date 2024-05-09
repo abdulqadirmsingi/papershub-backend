b8e7c672ec6434a9aaec0274c41fe4b2be03b0eb
Abdulqadir Mohammed <133521025+abdulqadirmsingi@users.noreply.github.com>
2024-05-09 22:56:56 +0300

    Update settings.py

--- paper/settings.py
+++ paper/settings.py
@@ -29,12 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS_ALLOW = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5502',
-    'http://localhost:5502'
-]
+# CORS_ALLOWED_ORIGINS_ALLOW = True
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:5502',
+#     'http://localhost:5502'
+# ]
 
 
 # Application definition
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
